37f0e0f59367af5677e1edb06dfa55f480789ef2
jkliebana <jcliebana@outlook.es>
2017-01-13 11:46:54 +0100

    Creacion de archivo .travis.yml y cambio de proyecto

---
+++ src/tests/FuncionSuma.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class Sum{
+	public static int sum(int a, int b){
+		//Cambio en master para mostrar conflicto
+		
+		return a+b;
+	}
+	
+}
---
+++ src/tests/Test2.java
@@ -0,0 +1,21 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test2 {
+	
+	@Test
+	public void testSum2Number_1(){
+		//Conflicto resuelto
+		assertEquals(2, 3-1);
+
+
+	}
+		
+	int res(int a, int b){
+		return a-b;
+	}
+
+}
